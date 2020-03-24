04da04ec3f4a5779b714c2d9c262a4db779ef8ee
Vincent Picavet <vincent.picavet@oslandia.com>
2020-03-24 19:29:51 +0100

    Do not use alias

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 wget https://www.gouvernement.fr/info-coronavirus -O info-coronavirus.html
 pandoc -f html -t markdown -s info-coronavirus.html  | grep -Ev ":::|\<div\>|\</div\>" > info-coronavirus.md
 git add info-coronavirus.md
-git ci -m "New official update"
+git commit -m "New official update"
 git push
