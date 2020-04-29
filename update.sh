62ffa61bed14acaff856cc10ee0657f231d1f997
Vincent Picavet <vincent.picavet@oslandia.com>
2020-04-29 13:36:38 +0200

    Commit both pages at the same time to keep github actions happy

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 wget https://www.gouvernement.fr/info-coronavirus -O info-coronavirus.html
 pandoc -f html -t markdown -s info-coronavirus.html  | grep -Ev ":::|\<div\>|\</div\>" > info-coronavirus.md
+wget https://www.gouvernement.fr/info-coronavirus/strategie-de-deconfinement -O strategie-de-deconfinement.html
+pandoc -f html -t markdown -s strategie-de-deconfinement.html |  grep -Ev ":::|\<div\>|\</div\>" > strategie-de-deconfinement.md
 git add info-coronavirus.md
+git add strategie-de-deconfinement.md
 git commit -m "New official update"
 git push
